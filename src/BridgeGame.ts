b243462254e449c45ad6955fa788e971bb70aa00
craftingmod <alyac1234@gmail.com>
2022-11-20 20:12:11 +0900

    Chore: Basic Template Commit

---
+++ src/BridgeGame.ts
@@ -0,0 +1,14 @@
+export default class BridgeGame {
+  /**
+   * 사용자가 칸을 이동할 때 사용하는 메서드
+   */
+  public move() {
+
+  }
+  /**
+   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+   */
+  public retry() {
+
+  }
+}
---
+++ src/BridgeMaker.ts
@@ -0,0 +1,9 @@
+/**
+ * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
+ * @param size 다리의 길이
+ * @param generateRandomNumber 무작위 값을 생성해주는 함수
+ * @returns 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
+ */
+export function makeBridge(size: number, generateRandomNumber: () => number): Array<"U" | "D"> {
+  return ["U"]
+}
